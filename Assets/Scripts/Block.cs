20e17cbd2b6d0a2a1dcc7a0715003ae61b116a6e
crodociles <lukefroud86@gmail.com>
2017-12-18 15:52:06 +0000

    inital commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public int score = 0;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy block
+        Destroy(gameObject);
+        score++;
+    }
+}
---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+    public float speed = 150;
+
+    void FixedUpdate()
+    {
+        //Get horizontal input
+        float h = Input.GetAxisRaw("Horizontal");
+        //Set velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
