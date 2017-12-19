7d2227486e761fa6dbad7ea7423b19471c8b9c1a
crodociles <lukefroud86@gmail.com>
2017-12-19 00:22:23 +0000

    Added Lives, Scoring and start/end procedures

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,26 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
     //Movement Speed
     public float speed = 100.0f;
+    public bool gameInProgress;
+    public static int numLives = 3;
+    public static int score = 0;
+    public Transform livesTarget;
+    public Transform scoreTarget;
+    public GameObject racket;
+    public float racketXPosition;
 
     float hitFactor(Vector2 ballPos, Vector2 racketPos,
                 float racketWidth)
     {
         // ascii art:
         //
-        // 1  -0.5  0  0.5   1  <- x value
-        // ===================  <- racket
+        // -1  -0.5  0  0.5   1  <- x value
+        //  ===================  <- racket
         //
         return (ballPos.x - racketPos.x) / racketWidth;
     }
 
     // Use this for initialization
     void Start () {
-        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
-	}
+        //Display initial score & lives
+        livesTarget.GetComponent<Text>().text = "Lives: " + numLives;
+        scoreTarget.GetComponent<Text>().text = "Score: " + score;
+    }
+
+    private void Update()
+    {
+        //press space to start game
+        if (Input.GetKeyDown(KeyCode.Space) && !gameInProgress)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+            gameInProgress = true;
+        }
+
+        //if ball passes y-position of paddle
+        if(this.GetComponent<Transform>().position.y < -100)
+        {
+            //Reload
+            SceneManager.LoadScene("arkanoid");
+            gameInProgress = false;
+
+            //take a life, if no more lives game over
+            if (numLives > 0)
+            {
+                numLives--;
+                livesTarget.GetComponent<Text>().text = "Lives: " + numLives;
+                score = 0;
+            }
+            else
+            {
+                gameOver();
+            }
+        }
+
+        if (!gameInProgress)
+        {
+            racketXPosition = racket.GetComponent<Transform>().position.x;
+            transform.position =  new Vector2(racketXPosition,-86.27f);
+        }
+        //Display score
+        scoreTarget.GetComponent<Text>().text = "Score: " + score;
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -38,5 +87,21 @@ public class Ball : MonoBehaviour {
             // Set Velocity with dir * speed
             GetComponent<Rigidbody2D>().velocity = dir * speed;
         }
+
+        //add score depending on colour of block
+        if (col.gameObject.name.Contains("block"))
+        {
+            score++;
+            speed += 2;
+        }
+    }
+
+    void gameOver()
+    {
+        //Reset vars
+        numLives = 3;
+        gameInProgress = false;
+        score = 0;
+        speed = 100.0f;
     }
 }
